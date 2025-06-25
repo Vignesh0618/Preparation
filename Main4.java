ddd11c26898248ea94bea202889e4b3d49d10824
Vignesh0618 <vijayvignesh88382@gmail.com>
2025-06-25 17:36:07 +0530

    new java4 file

---
+++ Main4.java
@@ -0,0 +1,24 @@
+package Preparation;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+               int n=sc.nextInt();
+               for(int i=0;i<n;i++){
+                for(int j=0;j<=2*n-1;j++){
+                    System.out.print(" ");
+                }
+                for(int j=0;j<=n;j++){
+                    System.out.print("* ");
+                }
+                for(int j=0;j<=2*n-1;j++){
+                    System.out.print(" ");
+                }
+
+                System.out.println(" ");
+               }
+        }
+    }
+}
