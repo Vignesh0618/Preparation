e8805d03e4f8b0a0d2d893e2527d6143824e19cc
VIGNESH V B <122109201+Vignesh0618@users.noreply.github.com>
2025-06-25 17:22:43 +0530

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Preparation;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+               int n=sc.nextInt();
+               for(int i=0;i<n;i++){
+                for(int j=0;j<=i;j++){
+                    System.out.print("* ");
+                }
+                System.out.println(" ");
+               }
+        }
+    }
+}
